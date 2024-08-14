fcb9c3ec8a5d9be01900be10ae9c1c1e9d6b146f
georgewrmarshall <george.marshall@consensys.net>
2024-08-14 15:01:59 -0700

    chore: removing default stories folder and adding button package stories

---
+++ packages/button/src/Button.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '.';
+
+const meta: Meta<typeof Button> = {
+  title: 'Components/Button',
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const DefaultStory: Story = {
+  args: {
+    children: 'Default Button',
+  },
+  name: 'Default',
+};
